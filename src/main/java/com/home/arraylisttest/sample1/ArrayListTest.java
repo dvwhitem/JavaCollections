6dbe24aee949ad3374581bdfdd6bf4431062474d
Vitaliy <dvwhitem@gmail.com>
2015-01-26 22:58:45 +0200

    There is first simple ArrayList

---
+++ src/main/java/com/home/arraylisttest/sample1/ArrayListTest.java
@@ -0,0 +1,18 @@
+package com.home.arraylisttest.sample1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vitaliy on 19.01.15.
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.add(6, 60);
+        System.out.println("list = " + list);
+        System.out.println("list.size() = " + list.size());
+    }
+}
